f4ffc0ccab41c46cc82e6deb4fc7fc7ff51f6387
LamboGasimbo63 <juandlopez01@hotmail.com>
2021-10-28 00:07:01 -0500

    Started working with GSAP in the logo

---
+++ src/js/animations.js
@@ -0,0 +1,6 @@
+// import gsap from 'gsap';
+gsap.to("#loader-main-logo .full-tyre", {duration: 3, rotate: 360, transformOrigin: "50% 50%", ease: "power2.out"});
+gsap.to("#navbar-main-logo .full-tyre", {duration: 2, rotate: 360, transformOrigin: "50% 50%", ease: "none", repeat: -1});
+
+// gsap.to(".full-tyre-mask #full-tyre", {duration: 5, rotate: 355, transformOrigin: "50% 50%"});
+
