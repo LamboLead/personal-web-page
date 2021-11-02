58d539731366cecd32389484351b7aa0a147b219
LamboGasimbo63 <juandlopez01@hotmail.com>
2021-11-01 21:39:25 -0500

    Animated logo and started animation for about-me

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,6 +1,91 @@
-// import gsap from 'gsap';
-gsap.to("#loader-main-logo .full-tyre", {duration: 3, rotate: 360, transformOrigin: "50% 50%", ease: "power2.out"});
-gsap.to("#navbar-main-logo .full-tyre", {duration: 2, rotate: 360, transformOrigin: "50% 50%", ease: "none", repeat: -1});
+// Rotating wheel
 
-// gsap.to(".full-tyre-mask #full-tyre", {duration: 5, rotate: 355, transformOrigin: "50% 50%"});
+let target = "#navbar-main-logo .full-tyre";
+let proxy = {rotation: 0};
+let rotationSetter = gsap.quickSetter(target, "rotate");
+let clamp = gsap.utils.clamp(-360, 360);
 
+
+ScrollTrigger.create({
+  // onToggle: (self) => {
+  //   console.log("I was toggled");
+  // },
+  onUpdate: (self) => {
+    // let degrees = clamp(Math.floor(self.getVelocity()));
+    let degrees = Math.floor(self.getVelocity()) / 2;
+    // console.log(degrees);
+    if (Math.abs(degrees) > Math.abs(proxy.rotation)) {
+      proxy.rotation = degrees;
+      gsap.to(proxy, {
+        rotation: 0, duration: 3, ease: "power4.out", overwrite: true,
+        onUpdate: () => {
+          rotationSetter(proxy.rotation)
+        }
+      });
+    }
+  }
+});
+
+gsap.set(target, {transformOrigin: "50% 50%"});
+
+// Snapping scrolling
+
+// About-me section
+// let tline = gsap.timeline({
+//   scrollTrigger: {
+//     markers: true,
+//     trigger: "#about-me",
+//     start: "top top",
+//     end: "bottom bottom",
+//     scrub: true,
+//     pin: true,
+//     anticipatePin: 2
+//   }
+// });
+// tline.to("#skillset", {width: 0});
+
+
+
+// ScrollTrigger.create({
+//   markers: true,
+//   trigger: "#about-me",
+//   start: "top top",
+//   end: "bottom bottom",
+//   pin: true,
+//   anticipatePin: 2
+// });
+
+// gsap.to("#skillset", {
+//   scrollTrigger: {
+//     markers: {startColor: "yellow", endColor: "blue"},
+//     trigger: "#about-me",
+//     start: "25% center",
+//     end: "bottom bottom",
+//     onEnter: () => {
+//       gsap.to()
+//     }
+//   },
+//   x: "40%",
+//   duration: 2
+// });
+
+ScrollTrigger.create({
+  markers: {startColor: "yellow", endColor: "blue"},
+  trigger: "#about-me",
+  start: "25% center",
+  end: "bottom bottom",
+  onEnter: () => {
+    gsap.to("#skillset", {
+      duration: 2,
+      onStart: (this) => {
+        this.addClass()
+      }
+    });
+  },
+  onEnterBack: () => {
+    gsap.to("#skillset", {
+      height: 100,
+      duration: 2
+    });
+  }
+});
