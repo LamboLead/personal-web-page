c202068d6af547967f291cfc60cfdd956cd31054
LamboGasimbo63 <juandlopez01@hotmail.com>
2021-11-10 23:20:28 -0500

    Added snap scrolling and fixed about-me

--- src/js/animations.js
+++ src/js/animations.js
@@ -11,9 +11,7 @@ ScrollTrigger.create({
   //   console.log("I was toggled");
   // },
   onUpdate: (self) => {
-    // let degrees = clamp(Math.floor(self.getVelocity()));
     let degrees = Math.floor(self.getVelocity()) / 3;
-    // console.log(degrees);
     if (Math.abs(degrees) > Math.abs(proxy.rotation)) {
       proxy.rotation = degrees;
       gsap.to(proxy, {
@@ -43,8 +41,6 @@ gsap.set(target, {transformOrigin: "50% 50%"});
 //   }
 // });
 // tline.to("#skillset", {width: 0});
-
-
 
 // ScrollTrigger.create({
 //   markers: true,
@@ -76,16 +72,45 @@ ScrollTrigger.create({
   end: "bottom bottom",
   pin: true,
   onUpdate: (self) => {
-    if (self.progress > 0.5) {
-      document.querySelector("#skillset").classList.remove("is-section-contracted");
-      document.querySelector("#who-am-i").classList.add("is-section-contracted");
+    if (self.progress > 0.6) {
+      document.querySelector("#skillset").classList.remove("is-skillset-contracted");
+      document.querySelector("#who-am-i").classList.add("is-about-me-contracted");
     } else {
-      document.querySelector("#skillset").classList.add("is-section-contracted");
-      document.querySelector("#who-am-i").classList.remove("is-section-contracted");
-
+      document.querySelector("#skillset").classList.add("is-skillset-contracted");
+      document.querySelector("#who-am-i").classList.remove("is-about-me-contracted");
     }
-    // console.log(self.progress.toFixed(1));
   }
-  // toggleActions: "play none none reverse",
-  // toggleClass: {targets: ["#skillset"], className: "is-section-contracted"}
+});
+
+// Snap scrolling
+
+const sections = document.querySelectorAll("#main-section .single-section, .double-section");
+
+function goToSection(section) {
+  // console.log(section);
+  gsap.to(window, {
+    scrollTo: {y: section, autoKill: false},
+    duration: 0.5,
+    delay: 0.5
+  });
+  
+  // anim && anim.restart();
+}
+
+sections.forEach((section) => {
+  // const intoAnim = gsap.from(section.querySelector(".right-col"), {yPercent: 50, duration: 1, paused: true});
+  let id = `#${section.id}`;
+  console.log(id);
+  
+  ScrollTrigger.create({
+    // markers: true,
+    trigger: section,
+    start: "top+=4% bottom",
+    end: "bottom-=4% top+=1",
+    // onEnter: () => goToSection(section, intoAnim),
+    onEnter: () => goToSection(id),
+    onEnterBack: () => goToSection(id)
+    // onEnter: () => console.log("I entered!", id),
+    // onEnterBack: () => console.log("I entered back!", id)
+  });
 });
